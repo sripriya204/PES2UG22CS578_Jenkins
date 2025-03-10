6ccaf9e671b9bdfd03eb00099546c48475d90bfc
Sripriya Addanki <sripriyaaddanki24@gmail.com>
2025-03-10 16:02:52 +0530

    made a change

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, this is a new\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
