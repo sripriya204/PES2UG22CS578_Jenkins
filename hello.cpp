96f2d7e966fbbec09eca7133888368e7731057b6
Sripriya Addanki <sripriyaaddanki24@gmail.com>
2025-03-11 10:44:51 +0530

    Added hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, this is a new\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
+
